e6a218cdd3de4d706849c1071bec1035a80d986f
Danil Korotenko <danil.korotenko@gmail.com>
2023-12-24 18:23:08 +0200

    Sample project added.

---
+++ IOKitToolsObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IOKitToolsObjC
+//
+//  Created by Danil Korotenko on 12/24/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
