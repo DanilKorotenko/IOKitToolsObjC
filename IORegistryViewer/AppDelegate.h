2f5d05657165bfc5afbef788be0e97c4686a5d5f
Danil Korotenko <danil.korotenko@gmail.com>
2024-01-01 16:11:31 +0200

    IORegistryViewer initial commit.

---
+++ IORegistryViewer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  IORegistryViewer
+//
+//  Created by Danil Korotenko on 1/1/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ IORegistryViewer/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  IORegistryViewer
+//
+//  Created by Danil Korotenko on 1/1/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
